17f37aff3e80e3ffef0e824e0fd89bbf28341789
Christopher Cruz <chris1cruz@gmail.com>
2013-03-08 12:13:19 -0600

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,17 @@ class UsersController < ApplicationController
   end
 
   def new
-      @user = User.new
+    @user = User.new
   end
-  
+
   def create
-      @user = User.new(params[:user])
-      if @user.save
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
-      else
-        render 'new'
-      end
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
 end
